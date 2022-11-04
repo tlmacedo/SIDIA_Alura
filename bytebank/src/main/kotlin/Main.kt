2879186012ae8b4306ee3567a961ea1643c3bfe4
tlmacedo <tl.macedo@hotmail.com>
2022-11-04 17:17:28 -0400

    3.04 - Implementando depósito

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -1,21 +1,48 @@
 fun main() {
     println("Bem vindo ao Bytebank\n")
 
-//    val contaThiago = Conta()
-//    contaThiago.titular = "Thiago Macedo"
-//    contaThiago.numero = 1000
-//    contaThiago.saldo = 200.0
-//    println(contaThiago.titular)
-//    println(contaThiago.numero)
-//    println(contaThiago.saldo)
-//
-//    val contaFran = Conta()
-//    contaFran.titular = "Fran"
-//    contaFran.numero = 1001
-//    contaFran.saldo = 300.0
-//    println(contaFran.titular)
-//    println(contaFran.numero)
-//    println(contaFran.saldo)
+    val contaThiago = Conta()
+    contaThiago.titular = "Thiago Macedo"
+    contaThiago.numero = 1000
+    contaThiago.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+
+    println("Titular: ${contaThiago.titular}")
+    println("Numero da conta: ${contaThiago.numero}")
+    println("Saldo: R$ ${contaThiago.saldo}")
+
+    println("Titular: ${contaFran.titular}")
+    println("Numero da conta: ${contaFran.numero}")
+    println("Saldo: R$ ${contaFran.saldo}")
+
+
+    println("Depositando na conta do Thiago")
+    deposita(contaThiago, 55.0)
+    deposita(contaFran, 26.0)
+
+
+
+}
+
+fun deposita(conta: Conta, valor: Double) {
+    conta.saldo += valor
+    println("Saldo atualizada do(a) ${conta.titular} é de R$ ${conta.saldo}")
+}
+
+class Conta {
+
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+
+}
+
+fun testaCopiasEReferencias() {
 
     val numeroX = 10
     var numeroY = numeroX
@@ -34,16 +61,8 @@ fun main() {
     println("titular conta João: ${contaJoao.titular}")
     println("titular conta Maria: ${contaMaria.titular}")
 
-
 }
 
-class Conta {
-
-    var titular = ""
-    var numero = 0
-    var saldo = 0.0
-
-}
 
 //    for (i in 1 ..10 step 2) {
 //
