800126d62e45e79fe2d056009682c6a09c48d14a
tlmacedo <tl.macedo@hotmail.com>
2022-11-08 11:50:35 -0400

    4.06 - Encapsulamento com getter e setter

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -1,24 +1,26 @@
+import java.util.DoubleSummaryStatistics
+
 fun main() {
     println("Bem vindo ao Bytebank\n")
 
     val contaThiago = Conta()
-    contaThiago.titular = "Thiago Macedo"
-    contaThiago.numero = 1000
-    contaThiago.saldo = 200.0
+    contaThiago.setTitular("Thiago Macedo")
+    contaThiago.setNumero(1000)
+    contaThiago.setSaldo(200.0)
 
     val contaFran = Conta()
-    contaFran.titular = "Fran"
-    contaFran.numero = 1001
-    contaFran.saldo = 300.0
+    contaFran.setTitular("Fran")
+    contaFran.setNumero(1001)
+    contaFran.setSaldo(300.0)
 
 
-    println("Titular: ${contaThiago.titular}")
-    println("Numero da conta: ${contaThiago.numero}")
-    println("Saldo: R$ ${contaThiago.saldo}")
+    println("Titular: ${contaThiago.getTitular()}")
+    println("Numero da conta: ${contaThiago.getNumero()}")
+    println("Saldo: R$ ${contaThiago.getSaldo()}")
 
-    println("Titular: ${contaFran.titular}")
-    println("Numero da conta: ${contaFran.numero}")
-    println("Saldo: R$ ${contaFran.saldo}")
+    println("Titular: ${contaFran.getTitular()}")
+    println("Numero da conta: ${contaFran.getNumero()}")
+    println("Saldo: R$ ${contaFran.getSaldo()}")
 
     println()
     contaThiago.deposita(55.0)
@@ -36,65 +38,97 @@ fun main() {
 
 class Conta {
 
-    var titular = ""
-    var numero = 0
-    var saldo = 0.0
-
+    private var titular: String = ""
+    private var numero: Int = 0
+    private var saldo: Double = 0.0
 
     fun imprimeSaldo() {
         println("Saldo atualizada do(a) $titular é de R$ $saldo")
     }
 
     fun deposita(valor: Double) {
+        if (!validaValor(valor)) return
         println("Depositando R$ $valor na conta do $titular.")
         saldo += valor
         imprimeSaldo()
     }
 
-    fun saca(valor: Double) {
+    fun saca(valor: Double): Boolean {
+        if (!validaValor(valor)) return false
         println("Sacando R$ $valor na conta do $titular.")
         if (saldo >= valor) {
             saldo -= valor
             imprimeSaldo()
+            return true
         } else {
             println("Saldo R$ $saldo, é insuficiente para sacar R$ $valor")
         }
+        return false
     }
 
     fun transfere(valor: Double, destino: Conta) {
+        if (!validaValor(valor)) return
         println("Transferindo R$ $valor de: $titular para: ${destino.titular}")
-        if (saldo >= valor) {
-            saldo -= valor
-            destino.saldo += valor
+        if (saca(valor)) {
+            destino.deposita(valor)
             imprimeSaldo()
         } else {
             println("Saldo R$ $saldo, é insuficiente para transferir R$ $valor")
         }
     }
 
+    fun validaValor(valor: Double): Boolean {
+        return (valor > 0.0)
+    }
+
+    fun getTitular(): String {
+        return titular
+    }
+
+    fun setTitular(nome: String) {
+        if (nome.isEmpty() || nome.equals("")) return
+        titular = nome
+    }
+
+    fun getNumero(): Int {
+        return numero
+    }
+
+    fun setNumero(num: Int) {
+        if (num <= 0) return
+        numero = num
+    }
+
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+    fun setSaldo(valor: Double) {
+        saldo = valor
+    }
 
 }
 
-fun testaCopiasEReferencias() {
-
-    val numeroX = 10
-    var numeroY = numeroX
-    numeroY++
-
-    println("numeroX $numeroX")
-    println("numeroY $numeroY")
-
-
-    val contaJoao = Conta()
-    contaJoao.titular = "João"
-
-    var contaMaria = contaJoao
-    contaMaria.titular = "Maria"
-
-    println("titular conta João: ${contaJoao.titular}")
-    println("titular conta Maria: ${contaMaria.titular}")
-
-}
+//fun testaCopiasEReferencias() {
+//
+//    val numeroX = 10
+//    var numeroY = numeroX
+//    numeroY++
+//
+//    println("numeroX $numeroX")
+//    println("numeroY $numeroY")
+//
+//
+//    val contaJoao = Conta()
+//    contaJoao.titular = "João"
+//
+//    var contaMaria = contaJoao
+//    contaMaria.titular = "Maria"
+//
+//    println("titular conta João: ${contaJoao.titular}")
+//    println("titular conta Maria: ${contaMaria.titular}")
+//
+//}
 
 
 //    for (i in 1 ..10 step 2) {
