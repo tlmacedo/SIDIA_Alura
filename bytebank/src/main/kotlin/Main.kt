3c0ae2c02db991f8c9360ba5480217378e2d616e
thiagomacedo <tl.macedo@hotmail.com>
2022-11-14 20:26:09 -0400

    Revert "02:1.4 - Organizando o projeto."
    This reverts commit cb41ef5482650a00f86a24dcf03cb0f7c31cba75.

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -2,17 +2,6 @@ fun main() {
 
     println("Bem vindo ao Bytebank\n")
 
-    val thiago = Funcionario(
-        nome = "Thiago Macedo",
-        cpf = "123.456.789.00",
-        salario = 5000.0
-    )
-
-    println("nome: ${thiago.nome}")
-    println("cpf: ${thiago.cpf}")
-    println("salario: R$${thiago.salario}")
-    println("bonificação: R$${thiago.bonificacao()}")
-
-    //testaComportamentoConta()
+    testaComportamentoConta()
 
 }
