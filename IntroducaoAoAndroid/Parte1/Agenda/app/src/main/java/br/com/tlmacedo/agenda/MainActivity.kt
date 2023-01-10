b516b609280f0400cd105ac272c760e091b57d78
thiagomacedo <tl.macedo@hotmail.com>
2023-01-10 09:06:22 -0400

    Android_01:2.03 - Iniciando a implementacao de layout de lista.

--- IntroducaoAoAndroid/Parte1/Agenda/app/src/main/java/br/com/tlmacedo/agenda/MainActivity.kt
+++ IntroducaoAoAndroid/Parte1/Agenda/app/src/main/java/br/com/tlmacedo/agenda/MainActivity.kt
@@ -3,14 +3,23 @@ package br.com.tlmacedo.agenda
 import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
-import androidx.annotation.ContentView
+import java.util.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var alunos: List<String> = Arrays.asList("Alex","Fran","Jose")
+        var primeiroAluno: TextView = findViewById(R.id.textView)
+        var segundoAluno: TextView = findViewById(R.id.textView2)
+        var terceiroAluno: TextView = findViewById(R.id.textView3)
+
+        primeiroAluno.setText(alunos.get(0))
+        segundoAluno.setText(alunos.get(1))
+        terceiroAluno.setText(alunos.get(2))
+
+
     }
 
 
